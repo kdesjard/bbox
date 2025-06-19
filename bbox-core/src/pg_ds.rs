2a1c643e7a8059d4c3f39312b3c19032563d0bf9
kdesjard <kristian.desjardins@nrcan-rncan.gc.ca>
2025-06-19 16:36:36 +0000

    on connect, set the search_path if configured and then set schemas in PgDatasource to be used later

--- bbox-core/src/pg_ds.rs
+++ bbox-core/src/pg_ds.rs
@@ -1,6 +1,6 @@
 use crate::config::DsPostgisCfg;
 use log::info;
-use sqlx::postgres::{PgPool, PgPoolOptions};
+use sqlx::postgres::{PgConnectOptions, PgPool, PgPoolOptions};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -14,19 +14,28 @@ pub type Result<T> = std::result::Result<T, Error>;
 #[derive(Clone, Debug)]
 pub struct PgDatasource {
     pub pool: PgPool,
+    pub schemas: Vec<String>,
 }
 
 impl PgDatasource {
     pub async fn from_config(ds: &DsPostgisCfg, envvar: Option<String>) -> Result<Self> {
-        Self::new_pool(&envvar.unwrap_or(ds.url.clone())).await
+        Self::new_pool(&envvar.unwrap_or(ds.url.clone()), ds.search_path.clone()).await
     }
-    pub async fn new_pool(url: &str) -> Result<Self> {
+    pub async fn new_pool(url: &str, search_path: Option<String>) -> Result<Self> {
         info!("Connecting to {url}");
+        let connect_options = PgConnectOptions::new();
+        let schemas = if let Some(sp) = search_path {
+            info!("Setting search_path to {sp}");
+            connect_options.options([("search_path", sp.clone())]);
+            sp.split(',').map(str::to_string).collect()
+        } else {
+            vec!["public".to_string()]
+        };
         let pool = PgPoolOptions::new()
             .min_connections(0)
             .max_connections(8)
             .connect(url)
             .await?;
-        Ok(PgDatasource { pool })
+        Ok(PgDatasource { pool, schemas })
     }
 }
